91f6f47bfc786b3336fc46cd846b9bdc033b3642
BenC0 <suddensquirrel94@gmail.com>
2022-02-22 10:47:01 +0000

    Added get_images function

--- src/variation_1/gallery.js
+++ src/variation_1/gallery.js
@@ -1,3 +1,5 @@
+import norman from "../norman"
+
 export function build_image_str(img) {
     return `<img src="${img.src}" alt="${img.alt}">`
 }
@@ -8,7 +10,23 @@ export function build_gallery(images) {
     return `<div class="pah156-image-gallery">${htmlStr}</div>`
 }
 
+export function get_images() {
+    let images_selector = "#pdp-full-image"
+    if(norman.core.elementManagement.exists(images_selector)) {
+        let image_els = norman.core.elementManagement.getAll(images_selector)
+        let images = []
+        image_els.forEach((el, ind) => images.push({
+            src: el.getAttribute("src"),
+            alt: el.getAttribute("alt") || `Product image #${ind}`,
+        }))
+        return images
+    } else {
+        return false
+    }
+}
+
 export const gallery = {
+    get_images,
     build_gallery,
     build_image_str,
 }
