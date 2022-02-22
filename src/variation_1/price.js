a66a099b92511d769265aafa20ac3862c3e187a6
BenC0 <suddensquirrel94@gmail.com>
2022-02-22 12:54:34 +0000

    Added functions to price module to retrieve price info

--- src/variation_1/price.js
+++ src/variation_1/price.js
@@ -1,3 +1,5 @@
+import norman from "../norman"
+
 export function build_price(prices) {
     return `<div class="pah156-price" price_type="${prices.type}">
         <p class="saving">${prices.saving}</p>
@@ -7,8 +9,38 @@ export function build_price(prices) {
     </div>`
 }
 
+export function check_and_get_price(selector) {
+    if (norman.core.elementManagement.exists(selector)) {
+        return norman.core.elementManagement.get(selector).textContent.trim()
+    } else {
+        return ""
+    }
+}
+
+export function get_type() {
+    let type = "one-time-purchase"
+    if (norman.core.elementManagement.exists("#checkout-combo__offer-text-er")) {
+        type = "easy-repeat"
+    } else if (norman.core.elementManagement.exists(".pdp-offer-text__inner")) {
+        type = "sale"
+    }
+    return type
+}
+
+export function get_prices() {
+    let prices = {
+        type: get_type(),
+        now: check_and_get_price("#offerPrice"),
+        was: check_and_get_price(".pdp-price__was"),
+        saving: check_and_get_price(".pdp-price__you-save"),
+        normalised: check_and_get_price(".pdp-price__weight"),
+    }
+    return prices
+}
+
 export const price = {
-    build_price
+    get_prices,
+    build_price,
 }
 
 export default price
