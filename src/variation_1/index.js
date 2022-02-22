1c36189437af64f7dd483c0ba2789e80f2981eb3
BenC0 <suddensquirrel94@gmail.com>
2022-02-22 14:15:17 +0000

    Initial build

--- src/variation_1/index.js
+++ src/variation_1/index.js
@@ -2,22 +2,55 @@ import variationCSS from "./index.css";
 import norman from "../norman/index.js"
 import breadcrumbs from "./breadcrumbs.js";
 import gallery from "./gallery.js";
+import price from "./price.js";
+import title from "./title.js";
 
 function build_template(details) {
     let breadcrumbs_html = breadcrumbs.build_breadcrumbs(details.crumbs)
     let gallery_html = gallery.build_gallery(details.images)
+    let title_html = `<h1 class="pah156-title">${details.title}</h1>`
+    let price_html = price.build_price(details.prices)
     return `<div class="pah156-layout-pdp">
         ${breadcrumbs_html}
         ${gallery_html}
+        ${title_html}
+        ${price_html}
     </div>`
+}
+
+function get_details() {
+    return {
+        crumbs: [],
+        images: gallery.get_images(),
+        title: title.get_title(),
+        brand: title.get_brand(),
+        reviews: title.get_reviews(),
+        prices: prices.get_prices(),
+    }
 }
 
 function init() {
     norman.core.log("Variation 1")
     norman.core.track(Variant.name, "Loaded", true)
-    // Move product title
-    // Move product star rating
-    // Adjust product price style
+    norman.core.log("Getting details")
+    let details = get_details()
+    norman.core.log({
+        msg: "Details scraped",
+        details
+    })
+    norman.core.log("Building template")
+    let element = build_template(details)
+    norman.core.log({
+        msg: "Template built",
+        element
+    })
+    let targetSel = ".layout-pdp"
+    let meth = "beforeBegin"
+    norman.core.log(`Checking if "${targetSel}" exists`)
+    if(norman.core.elementManagement.exists(targetSel)) {
+        norman.core.log(`Inserting template "${meth}" "${targetSel}"`)
+        norman.core.elementManagement.add(element, meth, targetSel)
+    }
 }
 
 const Variant = {
