058972649ccb49b3d5b11c86b311f401dc2d8382
BenC0 <suddensquirrel94@gmail.com>
2022-02-22 09:22:10 +0000

    Added poll conditions to check for product title, star rating, and price

--- src/variation_1/index.js
+++ src/variation_1/index.js
@@ -1,9 +1,16 @@
 import variationCSS from "./index.css";
 import norman from "../norman/index.js"
 
+function move_title() {
+
+}
+
 function init() {
     norman.core.log("Variation 1")
     norman.core.track(Variant.name, "Loaded", true)
+    // Move product title
+    // Move product star rating
+    // Adjust product price style
 }
 
 const Variant = {
@@ -11,7 +18,12 @@ const Variant = {
     css: variationCSS,
     conditions: () => {
         let conditions = []
-        conditions.push(true)
+        // Check for product title
+        conditions.push(norman.core.elementManagement.exists(`.pdp-heading-ratings__title`))
+        // Check for product star rating
+        conditions.push(norman.core.elementManagement.exists(`[onclick="ui.scrollTo('#pr-review-snapshot');"]`))
+        // Check for product price
+        conditions.push(norman.core.elementManagement.exists(`[data-module="pdp_price"]`))
         norman.core.log({message: `Polling: Conditions`, conditions})
         let result = conditions.every(a => a)
         norman.core.log({message: `Polling: Result`, result})
